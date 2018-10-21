e6da969372fe87793df6d5d91d8fde0891c92a26
stevencoin <40004560+stevencoind@users.noreply.github.com>
2018-10-21 07:11:50 +0200

    Update install_stevencoin.sh

--- install-guide/install_stevencoin.sh
+++ install-guide/install_stevencoin.sh
@@ -4,8 +4,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git -y && sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y && sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y && sudo apt-get install libboost-all-dev -y && sudo apt-get install software-properties-common -y && sudo add-apt-repository ppa:bitcoin/bitcoin -y && sudo apt-get update && sudo apt-get install libdb4.8-dev libdb4.8++-dev -y && sudo apt-get install libminiupnpc-dev -y && sudo apt-get install libzmq3-dev -y && sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler  -y && sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y && sudo apt-get update 
 
 mkdir .stevencoin && cd .stevencoin && echo 'server=1
+daemon=1
 rpcuser=user
-listen=0
 rpcpassword=Ecc7022b3D87a77E128081dbA62b1550Aa48
 addnode=45.32.238.28
 ' > stevencoin.conf && cd ..
